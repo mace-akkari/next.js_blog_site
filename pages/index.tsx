ecde1beef8425084220f5e185d944ee3e44eeb20
Mace Akkari <mace.akkari@relyfe.com>
2022-11-01 12:01:37 +0000

    renamed path posts to post

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home = ({ posts }: Props) => {
       {/* posts here*/}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
-          <Link key={post._id} href={`/posts/${post.slug.current}`}>
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="border rounded-lg  group cursor-pointer overflow-hidden">
               <img
                 className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
